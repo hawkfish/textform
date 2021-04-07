516db3febdcbc55cf4325284d0ad46ef8c3aa8a7
Richard Wesley <hawkfish@electricfish.com>
2021-04-07 14:23:01 -0700

    Implement Cast

---
+++ tests/test_cast.py
@@ -0,0 +1,37 @@
+import unittest
+from context import *
+
+class TestCast(unittest.TestCase):
+
+    def assert_cast(self, input, type_, value):
+        s = txf.Add(None, input, value)
+        t = txf.Cast(s, input, type_)
+
+        self.assertEqual('cast', t.name(), )
+        self.assertIsNotNone(t.source())
+        self.assertEqual((input,), t.inputs())
+        self.assertEqual((), t.outputs())
+        self.assertEqual(type_, t.type())
+
+        self.assertEqual(type_, t.schema()[input]['type'])
+        self.assertEqual({input: type_(value) }, t.next())
+
+        return t
+
+    def test_cast_string(self):
+        self.assert_cast('Target', int, '10')
+
+    def test_cast_int(self):
+        self.assert_cast('Target', str, 10)
+
+    def test_root(self):
+        self.assertRaises(txf.TransformException, txf.Cast, None, 'Target', str)
+
+    def test_input_count(self):
+        s = txf.Add(None, 'Target', 1)
+        self.assertRaises(txf.TransformException, txf.Cast, s, ('Target', 'Target',), str)
+        self.assertRaises(txf.TransformException, txf.Cast, s, (), str)
+
+    def test_missing(self):
+        s = txf.Add(None, 'Target', 1)
+        self.assertRaises(txf.TransformException, txf.Cast, s, 'Missing', str)
---
+++ textform/cast.py
@@ -0,0 +1,31 @@
+from .common import TransformException
+from .transform import Transform
+
+class Cast(Transform):
+    def __init__(self, source, input, type_):
+        super().__init__('cast', (input,), (), source)
+
+        if not source:
+            raise TransformException(f"Can't Cast from missing input.")
+
+        if len(self._inputs) != 1:
+            raise TransformException(f"Wrong number of inputs to Cast {len(self._inputs)}.")
+
+        schema = source.schema()
+        if self.input() not in schema:
+            raise TransformException(f"Cast input field '{self.input()}' missing.")
+
+        self._type = type_
+
+    def input(self): return self._inputs[0]
+    def type(self): return self._type
+
+    def schema(self):
+        schema = super().schema()
+        schema[self.input()]['type'] = self._type
+        return schema
+
+    def next(self):
+        row = super().next()
+        row[self.input()] = self._type(row[self.input()])
+        return row
