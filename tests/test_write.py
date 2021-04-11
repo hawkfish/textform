c6777b65cdbbcdeb9e7d17de3c026fc48981117e
Richard Wesley <hawkfish@electricfish.com>
2021-04-11 10:02:45 -0700

    Add JSON Write support

--- tests/test_write.py
+++ tests/test_write.py
@@ -2,15 +2,35 @@ import unittest
 from context import *
 
 import io
+import json
+
+def expected_csv(header, count):
+    sep = ','
+    eol = '\n'
+    expected = [sep.join(header),]
+    expected.extend([sep.join(['Value', str(r)]) for r in range(count)])
+    expected = eol.join(expected)
+    expected += eol
+
+    return expected
+
+def expected_json(header, count):
+    return json.dumps([{header[0]: 'Value', header[1]: r} for r in range(count)])
+
+expected_factory = {
+    'csv': expected_csv,
+    'json': expected_json,
+}
 
 class TestWrite(unittest.TestCase):
 
-    def assert_write(self, count):
+    def assert_write(self, count, format='csv'):
+        config = {'format': format}
         outfile = io.StringIO(newline=None)
         s = txf.Add(None, 'String', 'Value')
         s = txf.Sequence(s, 'Row')
         s = txf.Limit(s, count)
-        t = txf.Write(s, outfile)
+        t = txf.Write(s, outfile, **config)
 
         self.assertEqual('write', t.name, )
         self.assertEqual(('String', 'Row',), t.inputs)
@@ -22,22 +42,16 @@ class TestWrite(unittest.TestCase):
 
         self.assertEqual(count, t.pull())
 
-        sep = ','
-        eol = '\n'
-        expected = [sep.join(['String', 'Row']),]
-        expected.extend([sep.join(['Value', str(r)]) for r in range(count)])
-        expected = eol.join(expected)
-        expected += eol
+        expected = expected_factory[format](t.inputs, count)
         self.assertEqual(expected, outfile.getvalue())
 
-    def test_write_empty(self):
+    def test_write_csv(self):
         self.assert_write(0)
-
-    def test_write_one(self):
         self.assert_write(1)
-
-    def test_write(self):
         self.assert_write(19)
+
+    def test_write_json(self):
+        self.assert_write(2, 'json')
 
     def test_missing_source(self):
         self.assertRaises(txf.TransformException, txf.Write, None, None)
--- textform/write.py
+++ textform/write.py
@@ -2,20 +2,56 @@ from .common import TransformException
 from .transform import Transform
 
 import csv
+import json
+
+class CSVWriter(csv.DictWriter):
+
+    def __init__(self, outfile, fieldnames):
+        super().__init__(outfile, fieldnames)
+        self.writeheader()
+
+    def writefooter(self):
+        pass
+
+class JSONWriter(object):
+
+    def __init__(self, outfile, fieldnames):
+        self._outfile = outfile
+        self.fieldnames = fieldnames
+
+        self._outfile.write('[')
+        self._sep = ''
+
+    def writerow(self, row):
+        self._outfile.write(self._sep)
+        self._sep = ', '
+        json.dump(row, self._outfile)
+
+    def writefooter(self):
+        self._outfile.write(']')
+
+writer_factory = {
+    'csv': CSVWriter,
+    'json': JSONWriter,
+}
 
 class Write(Transform):
-    def __init__(self, source, outfile):
-        if not source:
-            raise TransformException(f"Can't write from missing input.")
+    def __init__(self, source, outfile, **params):
+        super().__init__('write', source.layout if source else (), (), source)
 
-        super().__init__('write', source.layout, (), source)
+        self._requireSource()
 
-        self._writer = csv.DictWriter(outfile, self.inputs)
-        self._writer.writeheader()
+        format = params.get('format', 'csv')
+        if format not in writer_factory:
+            raise TransformException(f"Unknown {self.name} format: '{format}'")
+
+        self._writer = writer_factory[format](outfile, self.layout)
 
     def next(self):
         row = super().next()
         if row is not None:
             self._writer.writerow(row)
+        else:
+            self._writer.writefooter()
 
         return row
