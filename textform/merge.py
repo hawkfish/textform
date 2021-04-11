31a2f28320844318775c9c1d74f62ee6480e0a98
Richard Wesley <hawkfish@electricfish.com>
2021-04-11 15:20:54 -0700

    Convert Merge to use closures

--- textform/merge.py
+++ textform/merge.py
@@ -1,13 +1,28 @@
 from .common import TransformException
 from .transform import Transform
 
+def bind_merge(glue):
+
+    if callable(glue):
+        return glue
+
+    def merge(values):
+        nonlocal glue
+
+        return glue.join(values)
+
+    return merge
+
 class Merge(Transform):
     def __init__(self, source, inputs, output, glue=''):
         super().__init__('merge', inputs, (output,), source)
 
         self.glue = glue
+        self.merger = bind_merge(glue)
 
         self._requireOutputs(self.inputs)
+
+        self._typed = False
 
     def _schema(self):
         schema = super()._schema()
@@ -21,9 +36,11 @@ class Merge(Transform):
     def next(self):
         row = super().next()
         if row is not None:
-            value = self.glue.join([row[input] for input in self.inputs])
+            value = self.merger([row[input] for input in self.inputs])
             for input in self.inputs:
                 del row[input]
             row[self.output] = value
+            if not self._typed:
+                self.schema[self.output] = {'type': type(value)}
 
         return row
